3166bfa91ac9a0717360fe65215f414085e0fd65
Deidei4455 <ganuprauskas@gmail.com>
2024-11-29 15:24:49 +0200

    Funkcijos papildytos aprašymu

--- funkcijos.py
+++ funkcijos.py
@@ -1,11 +1,19 @@
 # didžiausios išlaidos arba max atlyginimas
 def maximumas(listas):
-    atlyginimai = []
+    """
+    Funkcija priima listą ir suranda didžiausią skaičių
+    tame liste, kuris yra liste,
+    pvz. kaip atlyginimas didžiausias, grąžina
+    didžiausią skaičių kaip rezultatą.
+    :param listas: list
+    :return: max
+    """
+    skaiciai = []
     maximum = []
     for vard, pareig, atlg in listas:
-        atlyginimai.append(atlg)
+        skaiciai.append(atlg)
     for elem in listas:
-        if max(atlyginimai) in elem:
+        if max(skaiciai) in elem:
             maximum.append(elem)
 
     return maximum
@@ -14,6 +22,12 @@ def maximumas(listas):
 # listuose esamų pajamų arba išlaidų suma
 
 def sumuok(listas):
+    """
+    funkcija priima listą ir gražina
+    skaičių sumą.
+    :param listas: list
+    :return: suma
+    """
     suma = 0
     for elem in listas:
         suma += elem[2]
@@ -22,6 +36,12 @@ def sumuok(listas):
 
 # mažiausiai uždirbantis-(ys)
 def minimumas(listas):
+    """
+    funkcija priima listą ir grąžina
+    mažiausią skaičių listo liste.
+    :param listas: list
+    :return: minimumas
+    """
     atlyginimai = []
     minimum = []
     for vard, pareig, atlg in listas:
@@ -37,6 +57,12 @@ def minimumas(listas):
 
 
 def vidurkis(listas):
+    """
+    funkcija priima sudėtinį listą ir grąžina
+    to listo skaičių vidurkį kaip rezultatą.
+    :param listas: list
+    :return: vidurkis
+    """
     suma = 0
     for elem in listas:
         suma += elem[2]
